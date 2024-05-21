9f8151de1ff4b5792366344b2f83cf160fbe92a3
dimiitras <dimitras@thevtool.com>
2024-05-21 15:16:45 +0300

    Added Fifo_synchronous

---
+++ FIFO/Fifo_synchronous/fifo_synch.v
@@ -0,0 +1,155 @@
+module fifo_sync (
+clk,
+rst,
+w_en,
+r_en,
+wdata,
+full,
+empty,
+rdata);
+
+
+parameter MEMORY_WIDTH = 4;
+parameter MEMORY_DEPTH = 4;
+
+parameter ADDRESS_SIZE;
+
+input clk;
+input rst;
+input w_en;
+input r_en;
+input [(MEMORY_WIDTH-1):0] wdata;
+
+output full;
+output empty;
+output [(MEMORY_WIDTH-1):0] rdata;
+
+
+
+
+//Counter for w_ptr
+
+wire cw_en ;
+assign cw_en = (w_en & (!full));
+
+reg [(ADDRESS_SIZE-1):0] count_w_in;
+wire [(ADDRESS_SIZE-1):0] w_ptr; /*count_out*/
+
+
+wire w_max;
+assign w_max = (w_ptr == (MEMORY_DEPTH -1));
+
+wire w_wrap_up ;
+assign w_wrap_up = ( w_max);
+
+d_ff_async_en  #(.SIZE(ADDRESS_SIZE+1))
+ counter_write_address(.clk(clk),
+		       .rst(rst),
+		       .en(cw_en),
+		       .d(count_w_in),
+		       .q(w_ptr));
+	
+always@(*) begin
+	if(w_wrap_up) 
+		count_w_in = {ADDRESS_SIZE{1'b0}};
+	else 
+		count_w_in = w_ptr + 1'b1;
+	
+end
+
+
+
+
+//Counter for r_ptr
+
+wire [(ADDRESS_SIZE-1):0] r_ptr;
+
+wire cr_en = (r_en & (!empty));
+
+reg [(ADDRESS_SIZE-1):0] count_r_in;
+
+
+d_ff_async_en  #(.SIZE(ADDRESS_SIZE+1))
+ counter_read_address(.clk(clk),
+		      .rst(rst),
+		      .en(cr_en),
+		      .d(count_r_in),
+		      .q(r_ptr));
+	
+
+wire r_max;
+assign r_max = (r_ptr == (ADDRESS_SIZE -1));
+
+wire r_wrap_up ;
+assign r_wrap_up = (rst | r_max);
+
+always@(*) begin
+	if(r_wrap_up)
+		count_r_in = {ADDRESS_SIZE{1'b0}};
+	else
+		count_r_in = r_ptr + 1'b1;
+end
+
+
+
+
+
+//FULL signal generation
+
+wire D;
+assign D = (w_max & w_en);
+
+wire Q;
+
+wire eq;
+assign eq = (w_ptr == r_ptr);	
+
+d_ff_async_en  #(.SIZE(1'b1))
+	d_ff_full(.clk(clk),
+		  .rst(rst),
+		  .en((!eq)),
+		  .d(D),
+		  .q(Q)); 
+
+
+
+assign full = (eq & Q);
+
+
+
+
+//EMPTY signal generation
+
+assign empty = (!Q & eq);
+
+
+
+
+
+//Memory
+
+reg [(MEMORY_WIDTH -1) :0] memory [(MEMORY_DEPTH -1) :0];
+
+/*wire read;
+assign read = (r_en & (!FULL));*/
+
+
+
+always@(posedge clk) begin
+	if(cw_en) 
+		memory[w_ptr] <= wdata;		
+	
+end
+
+//Read 
+
+d_ff_async_en #(.SIZE(MEMORY_WIDTH))
+	read_out(.clk(clk),
+		 .rst(rst),
+		 .en((r_en & (!empty))),
+		 .d(memory[r_ptr]),
+		 .q(rdata));
+	
+
+
+endmodule
---
+++ FIFO/Fifo_synchronous/fifo_synch_tb.v
@@ -0,0 +1,119 @@
+`timescale 1ns/1ps
+//`define CLK_PERIOD 10
+
+
+module fifo_sync_tb ();
+
+parameter MEMORY_WIDTH = 4;
+parameter MEMORY_DEPTH = 4;
+
+parameter ADDRESS_SIZE = 2;
+
+parameter CLK_PERIOD = 10;
+
+integer i;
+
+reg clk;
+reg rst;
+reg w_en;
+reg r_en;
+reg [(MEMORY_WIDTH-1):0] wdata;
+
+wire full;
+wire empty;
+wire [(MEMORY_WIDTH-1):0] rdata;
+
+
+
+fifo_sync #(.MEMORY_WIDTH(MEMORY_WIDTH),
+	    .MEMORY_DEPTH(MEMORY_DEPTH),
+	    .ADDRESS_SIZE(ADDRESS_SIZE))
+  fifo_synchronous (.clk(clk),
+	    	   .rst(rst),
+	    	   .w_en(w_en),
+	    	   .r_en(r_en),
+	   	   .wdata(wdata),
+	    	   .full(full),
+	   	   .empty(empty),
+	    	   .rdata(rdata));
+
+initial
+begin
+	clk = 1'b0;
+	rst = 1'b1;
+	
+	w_en = 1'b0;
+	r_en = 1'b0;
+	
+	wdata = {MEMORY_WIDTH{1'b0}};
+	
+	#15;
+	
+	rst = 1'b0;
+	main;
+	
+	#1500; 
+	$stop;
+end
+
+
+always #(CLK_PERIOD/2)  clk = ~ clk;
+
+
+task main; 
+begin
+	
+	Write;
+	Read;
+end
+endtask
+
+
+
+  
+task Write;
+begin
+		w_en = 1'b1;
+		for(i=0; i < 3; i = i +1) begin
+		@(posedge clk)
+		begin
+			wdata = wdata + 1'b1;
+		end
+		end
+		#3;
+		w_en = 1'b0;
+
+end
+endtask
+
+task Read;
+begin
+		r_en = 1'b1;
+		#(CLK_PERIOD *3)
+		r_en = 1'b0;
+
+
+end
+endtask
+
+/*task Write_Read;
+begin
+
+	w_en = 1'b1;
+	@(posedge clk);
+	WR= 1;
+	@(posedge clk);
+	r_en = 1'b1;
+	@(posedge clk);
+	@(posedge clk);
+	w_en = 1'b1;
+	WR = 2;
+	@(posedge clk);
+	@(posedge clk);
+	r_en = 1'b1;
+end
+endtask*/
+
+
+
+endmodule
